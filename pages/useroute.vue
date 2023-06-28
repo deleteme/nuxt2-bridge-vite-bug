1237260b8ba79596fd919ce7a6cefa69d63c2703
Matt Garrett <mattga@gmail.com>
2023-06-28 15:03:12 -0700

    add useRoute example with @nuxtjs/composition-api

---
+++ pages/useroute.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>useRoute() demo</h1>
+    <p>Importing from @nuxtjs/composition-api or #app</p>
+    <h4>fullPath</h4>
+    <pre>{{fullPath}}</pre>
+    <h4>hash</h4>
+    <pre>{{hash}}</pre>
+    <h4>meta</h4>
+    <pre>{{meta}}</pre>
+    <h4>name</h4>
+    <pre>{{name}}</pre>
+    <h4>path</h4>
+    <pre>{{path}}</pre>
+    <h4>redirectedFrom</h4>
+    <pre>{{redirectedFrom}}</pre>
+  </div>
+</template>
+<script>
+import { defineComponent, useRoute } from '@nuxtjs/composition-api'
+/*import { useRoute, defineComponent } from '#app'*/
+export default defineComponent({
+  name: 'UseRoute',
+  setup() {
+    const route = useRoute();
+    const { fullPath, hash, meta, name, path, redirectedFrom } = route;
+
+    return { fullPath, hash, meta, name, path, redirectedFrom };
+  }
+});
+</script>
