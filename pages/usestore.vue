fb813920902d1d354451f09c726aaed717258ece
Matt Garrett <mattga@gmail.com>
2023-06-28 16:29:00 -0700

    add vuex store demo page

---
+++ pages/usestore.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>useStore() demo</h1>
+    <p>Importing from @nuxtjs/composition-api or #app</p>
+    <button type="button" @click="increment">
+      Increment
+    </button>
+    <input type="number" :value="counter" disabled />
+  </div>
+</template>
+<script>
+/*import { computed, defineComponent, useStore } from '@nuxtjs/composition-api'*/
+/*import { computed, useNuxtApp, defineComponent } from '#app'*/
+const useStore = () => useNuxtApp().$store;
+
+export default defineComponent({
+  name: 'UseStore',
+  setup() {
+    const store = useStore();
+    /*console.log('store', store);*/
+    const { state, dispatch } = store;
+    /*console.log('state.counter', state.counter);*/
+    const counter = computed(() => state.counter);
+    const increment = () => dispatch('increment');
+
+    return { counter, increment };
+  }
+});
+</script>
